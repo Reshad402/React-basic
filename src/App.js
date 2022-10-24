f76414e9b515fb1a962b41e945dc1f4381de8954
Reshad402 <mdrhr402@gmail.com>
2022-10-24 16:33:52 +0600

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
 import logo from './logo.svg';
 import './App.css';
 
+const num=100;
+const singer ={
+  name:'Sajib',
+  age: 76
+}
+const singerStyle ={
+  color:'red',
+  backgroundColor:'white'
+}
+
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+  
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+      <div className="container">
+      
+        {/* <img src={logo} className="App-logo" alt="logo" />
+        <h1>JSX</h1>
+        <div className="container">
+          <h3>Bello</h3>
+          <p>My is Hassan kore debo Massan</p>
+        </div>
+        <div className="total">
+          <h4>Total:{num+200}</h4>
+          <p style={singerStyle}>Name of the singer "{singer.name}" and his age is "{singer.age}"</p>
+          <p style={{color:'black',backgroundColor:'yellow'}}>Name of the singer "{singer.name}" and his age is "{singer.age}"</p>
+        </div> */}
+        
+
+
+
+        <Person nayok="Reshad" nayika="Afrina"></Person>
+        <Person nayok="Reshad" nayika="Shemanto"></Person>
+        <p>Rock mama</p>
+        <Friends actor="Fahad" actress="Shemanto"></Friends>
+        <Friends actor="Mishu" actress="Shemanto"></Friends>
+
+        </div>
+
       </header>
+    
     </div>
+
   );
 }
 
+function Person(props){
+  console.log(props)
+  return (
+    <div className='person'>
+      <h2>{props.nayok}</h2>
+      <p>Nayika is {props.nayika}</p>
+    </div>
+  )
+}
+  function Friends(props){
+    console.log(props)
+    return (
+      <div className="person2">
+        <h2>{props.actor}</h2>
+      <p>Nayika for 2nd is {props.actress}</p>
+      </div>
+    )
+  }
 export default App;
+
+
+
+
